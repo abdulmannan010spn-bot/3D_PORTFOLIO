95619c0e7fd55817ac038889135846a8c88a6c68
Abdul Mannan <abdulmannan010spn@gmail.com>
2026-08-11 15:53:46 +0530

    Techstack section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero"
 import LogoSection from "./Components/LogoSection"
 import Navbar from "./Components/Navbar"
 import Showcase from "./Components/Showcase"
+import Techstack from "./Components/Techstack"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <LogoSection />
       <FeatureCard />
       <Experience />
+      <Techstack />
     </main>
   )
 }
